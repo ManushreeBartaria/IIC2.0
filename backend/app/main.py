db8e8fef45daf9d50d91ba1ebfad5be10e04ef43
unknown <manushreebartaria07@gmail.com>
2025-09-07 23:13:37 +0530

    minor update

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(policememberroutes.router, prefix="/policeauth", tags=["Authentication","Add Police Member"])
+app.include_router(policememberroutes.router, prefix="/policeauth", tags=["Police Authentication"])
 app.include_router(firroutes.router, prefix="/fir", tags=["FIR Registration"])
 app.include_router(citizenroutes.router, prefix="/citizen", tags=["Citizen"])
 
