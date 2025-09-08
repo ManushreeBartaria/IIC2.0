d05ff1a89d74b7b3af73328fbcbc5726524154c6
unknown <manushreebartaria07@gmail.com>
2025-09-08 22:27:45 +0530

    tourist login

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.database.connection import engine, Base
 from app.models import Trial
 from app.models import PoliceStation, PoliceMember
-from app.api.routes import policememberroutes,firroutes,citizenroutes,governmentroutes
+from app.api.routes import policememberroutes,firroutes,citizenroutes,governmentroutes,touristroutes
 
 
 app = FastAPI()
@@ -13,6 +13,7 @@ app.include_router(policememberroutes.router, prefix="/policeauth", tags=["Polic
 app.include_router(firroutes.router, prefix="/fir", tags=["FIR Registration"])
 app.include_router(citizenroutes.router, prefix="/citizen", tags=["Citizen"])
 app.include_router(governmentroutes.router, prefix="/government", tags=["Government"])
+app.include_router(touristroutes.router, prefix="/tourist", tags=["Tourist Authentication"])
 
 @app.get("/")
 def read_root():
---
+++ backend/app/schemas/tourist.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class TouristCreate(BaseModel):
+
+    aadhar_no: Optional[str] = None
+    passport_no: Optional[str] = None
+    password: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class TouristResponse(BaseModel):
+    message: str
+
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class TouristAuth(BaseModel):
+    aadhar_no: Optional[str] = None
+    passport_no: Optional[str] = None
+    password: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class TouristAuthResponse(BaseModel):
+    access_token: str
+    token_type: str
+
+    model_config = {
+        "from_attributes": True
+    }
